d3f073a10edf4ebea738110edf9bdcc2cdd95837
Peter Brohan <peter.brohan@gmail.com>
2024-11-20 18:03:28 +0000

    Inital commit

---
+++ .eleventy.js
@@ -0,0 +1,27 @@
+const nunjucks = require("nunjucks");
+
+module.exports = function (eleventyConfig) {
+    // Set up a custom Nunjucks environment
+     const env = new nunjucks.Environment(
+        new nunjucks.FileSystemLoader([
+            "src/_includes",
+            "node_modules/govuk-frontend/dist"
+        ])
+    );
+
+    eleventyConfig.setLibrary("njk", env);
+    eleventyConfig.addPassthroughCopy("src/assets");
+    eleventyConfig.addPassthroughCopy("src/stylesheets");
+    eleventyConfig.addPassthroughCopy("src/javascripts");
+
+    return{
+        markdownTemplateEngine: "njk",
+        htmlTemplateEngine: "njk",
+        dataTemplateEngine: "njk",
+        dir: {
+            input: "src",
+            includes: "_includes",
+            output: "output",
+        },
+    };
+};
